2ca3bbf8e746683b9d6cf272e22d81bc77895768
rhyjudd <rhyjudd@gmail.com>
2019-10-30 14:44:41 -0500

    commiting files to the new branch

---
+++ Models/Certificate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainingHub.Models
+{
+    public class Certificate
+    {
+        public int CertificateID { get; set; }
+        public string CertTitle { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateAwarded { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateExpires { get; set}
+    }
+}
